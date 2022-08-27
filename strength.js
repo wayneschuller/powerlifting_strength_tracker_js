fd1190119e3142533a1bfb99a1c165365f743944
Wayne Schuller <wayne@schuller.id.au>
2022-08-27 21:40:13 +1000

    Big update: dynamic data, graphs top 5

--- strength.js
+++ strength.js
@@ -1,9 +1,8 @@
 // Global variables
 let workout_date_COL, completed_COL, exercise_name_COL, assigned_reps_COL, assigned_weight_COL, actual_reps_COL, actual_weight_COL, missed_COL, description_COL;
 
-let squatGraphData = []; // array of objects in charts.js friendly format (x, y, label)
-let deadliftGraphData = [];
-let benchGraphData = [];
+let rawLiftData = []; // Array of lift objects
+let processedData = []; // Array with one element per lift type of charts.js graph friendly data and special achievements 
 
 function readCSV () {
     let reader = new FileReader; 
@@ -17,16 +16,24 @@ function readCSV () {
             if (columnNames[0] === "user_name" && columnNames[1] === "workout_id") {
 
                 // console.log("this is bloc data CSV");
-                parseBlocCSV(data.data);  
+                parseBlocCSV(data.data);  // Loads the data into rawLiftData[]
 
             } else if (columnNames[0] === "Date" && columnNames[4] === "Pukie") {
 
                 // console.log("This is BTWB data CSV");
-                parseBtwbCSV(data.data);  
+                parseBtwbCSV(data.data);  // Loads the data into rawLiftData[]
 
             } else {
                 console.error("Did not detect CSV format. Currently we only process CSV data from Barbell Logic Online Coaching app and Beyond the Whiteboard app");
             }
+
+            // We now have the rawLiftData from various sources.
+            // Process that data into our processedData structure
+            processRawLiftData();
+
+            // Every element of processedData has a graphData array
+            // Sort our processed data so the most popular lift types are at the beginning
+            processedData.sort((a, b)=> b.graphData.length - a.graphData.length);
 
             // Draw the chart now we have data.
             let canvas = document.getElementById('myChart');
@@ -36,8 +43,51 @@ function readCSV () {
     reader.readAsBinaryString(fileInput.files[0]);
 }
 
-// Parse Beyond the Whiteboard app workout data CSV into the global graphData array
-// Assumes a PapaParse data format - array of rows, each row an array of items 
+function processRawLiftData() {
+
+    for (let i = 0; i < rawLiftData.length; i++) {
+        let index = processedData.findIndex(lift => lift.name === rawLiftData[i].name);
+        // console.log(`Index is ${JSON.stringify(index)}`);
+
+        if (index === -1) {
+            let processedLiftType = {
+                name: rawLiftData[i].name,
+                graphData: [],
+                best5RM: null,
+                best3RM: null,
+                best1RM: null
+            };
+            index = processedData.push(processedLiftType) - 1;
+        }
+        
+        let oneRepMax = estimateE1RM(rawLiftData[i].reps, rawLiftData[i].weight);
+        // Do we already have a processed same lift on this same date at the end of the array?
+        // FIXME: this should really search the whole array for the date anywhere
+        // console.log(`processedData[index].graphData: ${processedData[index].graphData}`); 
+
+        if (processedData[index].graphData.at(-1) && 
+            rawLiftData[i].date === processedData[index].graphData.at(-1).x) { 
+            // console.log(`We did this lift today already...`);
+            
+            if (oneRepMax > processedData[index].graphData.at(-1).y) {
+                // Get rid of the old process data so we can push a bigger one on
+                processedData[index].graphData.pop();
+            } else continue;
+        }
+
+        // Push a new graphData item for this lift type.
+        let label = '';
+        if (rawLiftData[i].reps === 1)
+            label = `Lifted 1@${rawLiftData[i].weight}kg`;
+        else
+            label = `Potential 1@${oneRepMax}kg from ${rawLiftData[i].reps}@${rawLiftData[i].weight}kg`;
+
+        processedData[index].graphData.push({x:rawLiftData[i].date, y: oneRepMax, label:`${label}`});
+    }
+    console.log(`We now have ${processedData.length} types of lifts`);
+}
+
+// Parse BTBW into our generic rawLiftData array structure
 function parseBtwbCSV(data) {
 
     let columnNames = data[0]; // header row
@@ -65,19 +115,9 @@ function parseBtwbCSV(data) {
       
     console.log(`Excellent BTWB data set header row.`);
 
-    // Back Squat
-    let lifts = data.filter(isBtwbLift, "Back Squat"); // filter for squat rows 
-    squatGraphData = lifts.map(processBtwbData); // convert every row best e1rm to chart.js style
-    
-    // Deadlift
-    lifts = data.filter(isBtwbLift, "Deadlift");  
-    deadliftGraphData = lifts.map(processBtwbData); // convert every row best e1rm to chart.js style
-
-    // Bench Press
-    lifts = data.filter(isBtwbLift, "Bench Press"); 
-    benchGraphData = lifts.map(processBtwbData); // convert every row best e1rm to chart.js style
-
-    //console.log(`Created graphData: ${JSON.stringify(graphData)}`);
+    data.shift(); // remove header row
+    data.forEach(parseBtwbRow);
+    console.log(`We have parsed ${rawLiftData.length} lifts of raw data. First one is ${JSON.stringify(rawLiftData[0].name)}`);
 }
 
 // Parse Barbell Logic app workout data CSV into the global graphData array
@@ -139,91 +179,40 @@ function parseBlocCSV(data) {
       
     console.log("Excellent BLOC data set header row.");
 
-    // Squat
-    let lifts = data.filter(isBlocLift, "Squat"); // get BLOC format squats with actual reps + weight
-    squatGraphData = lifts.map(processBlocData); // Process e1rms to chart.js style
-    squatGraphData = removeDupes(squatGraphData); 
+    data.shift(); // remove header row
+    data.forEach(parseBlocRow);
+    console.log(`We have parsed ${rawLiftData.length} lifts of raw data. First one is ${JSON.stringify(rawLiftData[0].name)}`);
 
-    // Deadlift
-    lifts = data.filter(isBlocLift, "Deadlift"); // get BLOC format deadlifts with actual reps + weight
-    deadliftGraphData = lifts.map(processBlocData); // Process e1rms to chart.js style
-    deadliftGraphData = removeDupes(deadliftGraphData); 
-
-    // Bench Press
-    lifts = data.filter(isBlocLift, "Bench Press"); // get BLOC format bench press with actual reps + weight
-    benchGraphData = lifts.map(processBlocData); // Process e1rms to chart.js style
-    benchGraphData = removeDupes(benchGraphData); 
-
-    // console.log(`CSV had ${data.length} rows with ${graphData.length} processed e1rm lifts.`);
 }
 
 
-// Take an array of graphData structs and if there are duplicates in a day keep only best lift
-// Useful for Bloc data which has one lift per row.
-// This code assumes only two lifts are entered per day (normally a topset and volume set)
-// and that these two lifts will be neighbours in the array.
-// FIXME: does it fail when 3 squats are recorded in the same day?
-function removeDupes(arr) {
+// Load the BLOC data as a liftEntry object into rawLiftData
+function parseBtwbRow(row) {
 
-    let output = [];
+    // console.log(`parseBtwbRow: ${JSON.stringify(row)}`);
 
-    // Start looping with the second item
-    // Compare current with previous pairs
-    // If date is the same, pick a winner then jump forward twice (note the extra i++)
-    // If the date is different just let the previous one be chosen 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i].x === arr[i-1].x) 
-            if (arr[i].y >= arr[i-1].y) {
-                output.push(arr[i]); 
-                i++;
-            } else {
-                output.push(arr[i-1]); 
-                i++; 
-            } 
-        else output.push(arr[i-1]); // the dates are not equal, push the previous one
+    if (!row || row[0] === null) {
+            console.log(`processBtwbRow skipping bad row: ${JSON.stringify(row)}`);
+            return; 
     }
 
-    return output;
-}
+    // Find the exercise name AKA type of lift in this row
+    let regex = /[a-zA-Z ]*/gm;
+    let result = regex.exec(row[1]); // Second column has the description - FIXME: use const column index format
+    let liftType = result[0].trim();
 
-// Look for squat data in this BTWB format row 
-// This is a filter function that will also be passed string with name of lift
-function isBtwbLift(row) {
-
-    console.log(`isBtwbLift ${this} passed: ${JSON.stringify(row)}`);
-
-    if (!row || row[0] === null) return false;
-
-    let result = row[description_COL].includes(this); 
-
-    console.log(`Did we find a ${this}? ${result}`);
-
-    return result;
-}
-
-// Convert a BTWB row of data to an object for charts.js graph with our e1rms over time
-function processBtwbData(row) {
-    let lifted_reps = 0;
-    let lifted_weight = 0;
-
-    // console.log(`processBtwbData passed: ${row}`);
-
-    if (row === undefined) {
-            console.error("row undefined?");
-            return []; // what to return when failure? null or []?
+    if (liftType === "") {
+        console.log("liftType was empty string, probably a metcon");
+        return;
     }
 
+    // Loop through the lifts of this session and push them all to rawLiftData
     let lifts = row[description_COL].split(/\r?\n/); // BTWB has newlines inside the one cell entry
-
-    // console.log(`Lifts is split into: ${JSON.stringify(lifts)}`);
-   
-    // Loop through the lifts of this session and find the best e1rm lift
-    let oneRepMax = 0;
     for (let lift of lifts) {
         // console.log(`Procesing lift ${lift}`);
 
         // Get number of reps
-        let regex = /^[0-9]+/gm;  // FIXME: does this work for 10+ reps?
+        let regex = /^[0-9]+/gm;  
         let result = regex.exec(lift);
         if (!result) continue;
         let curReps = parseInt(result[0]);
@@ -236,40 +225,41 @@ function processBtwbData(row) {
         let curWeight = parseInt(result[0].slice(0, result[0].length-2)); // Remove the kg off the end
         if (curWeight == 0) continue;
         
-        // console.log(`discovered curReps: ${curReps}, curWeight: ${curWeight}`);
-
-        let curMax = estimateE1RM(curReps, curWeight);
-        if (curMax >= oneRepMax) {
-            oneRepMax = curMax; 
-            lifted_reps = curReps;
-            lifted_weight = curWeight;
+        let liftEntry = {
+            name: liftType,
+            date: row[workout_date_COL],
+            reps: curReps,
+            weight: curWeight,
+            // FIXME: add units here
         }
-    }
 
-    if (oneRepMax === 0) {
-        console.error("We did not find any lifts (oneRepMax still 0)");
-        return [];
+        rawLiftData.push(liftEntry); // add to our collection of raw data
     }
-
-    // console.log(`lifted_reps: ${lifted_reps}, lifted weight: ${lifted_weight}, e1rm: ${oneRepMax}`); 
-    if (lifted_reps === 1)
-        return {x:row[workout_date_COL], y: oneRepMax, label:`Lifted ${lifted_reps}@${lifted_weight}kg`};
-    else
-        return {x:row[workout_date_COL], y: oneRepMax, label:`Potential 1@${oneRepMax}kg from ${lifted_reps}@${lifted_weight}kg`};
 }
 
 
-// Convert a BLOC row of data to an object for charts.js with e1rm
-// Old:
-// ['Wayne Schuller', '25079', '2021-06-16', 'true', 'workout', '', '3321.0', 'false', 'Bench Press', 'resistance', '', '3', 'standard', '3', 'standard', '118.0', 'standard', '', 'kg', '3', '3', '119.0']
-// New:
-// {x:'2021-06-16', y:130, label:'3@119kg'}
-function processBlocData(row) {
+// Load the BLOC data as a liftEntry object into rawLiftData
+function parseBlocRow(row) {
+
+    if (!row || row[0] === null) {
+            console.log(`parseBlocRow skipping bad row: ${JSON.stringify(row)}`);
+            return; 
+    }
+
+    // Give up on this row if it is not a completed workout
+    if (!row[completed_COL]) return false;
+
+    // Give up on this row if missed_COL is true 
+    if (row[missed_COL] === true) return false;
+
+    // Give up on this row if there is no assigned reps 
+    // Happens when coach leaves comments in the app
+    if (!row[assigned_reps_COL]) return false;
+
     let lifted_reps = row[assigned_reps_COL];
     let lifted_weight = row[assigned_weight_COL];
 
-    //console.log(`Processing row: ${row}`);
-    //console.log(`Processing top: lifted_reps: ${lifted_reps}, lifted weight: ${lifted_weight}`); 
+    // console.log(`Processing top: lifted_reps: ${lifted_reps}, lifted weight: ${lifted_weight}`); 
 
     // Override if there is an actual_reps and actual_weight as well
     // This happens when the person lifts different to what was assigned by their coach
@@ -277,15 +267,17 @@ function processBlocData(row) {
             lifted_reps = row[actual_reps_COL];
             lifted_weight = row[actual_weight_COL];
     }
-    
-    // Calculate 1RM for the set
-    let oneRepMax = estimateE1RM(lifted_reps, lifted_weight);
-    // console.log(`lifted_reps: ${lifted_reps}, lifted weight: ${lifted_weight}, e1rm: ${onerepmax}`); 
+   
+    if (lifted_reps === 0 || lifted_weight === 0) return;
 
-    if (lifted_reps === 1)
-        return {x:row[workout_date_COL], y: oneRepMax, label:`Lifted ${lifted_reps}@${lifted_weight}kg`};
-    else
-        return {x:row[workout_date_COL], y: oneRepMax, label:`Potential 1@${oneRepMax}kg from ${lifted_reps}@${lifted_weight}kg`};
+    let liftEntry = {
+        name: row[exercise_name_COL],
+        date: row[workout_date_COL],
+        reps: lifted_reps,
+        weight: lifted_weight,
+        // FIXME: add units here
+    }
+    rawLiftData.push(liftEntry); // add to our collection of raw data
 }
 
 // Return a rounded 1 rep max using Epley formula
@@ -302,82 +294,30 @@ function estimateE1RM(reps, weight) {
 }
 
 
-// Check a row of BLOC data to see if there is a valid squat lift in this row
-// The BLOC data format contains many rows that don't actually count as a lift
-// Filter function will also take a string with name of lift
-function isBlocLift(row) {
-
-    // Give up on this row if the parser has given us an empty element
-    if (!row) return false;
-
-    // Give up on this row if there is no date field (should never happen)
-    if (!row[workout_date_COL]) return false;
-
-    // Is it a squat? Give up if it is not a squat
-    if (row[exercise_name_COL] != this) return false;
-
-    // Give up on this row if it is not a completed workout
-    if (!row[completed_COL]) return false;
-
-    // Give up on this row if missed_COL is true 
-    if (row[missed_COL] === true) return false;
-
-    // Give up on this row if there is no assigned reps 
-    // Happens when coach leaves comments in the app
-    if (!row[assigned_reps_COL]) return false;
-
-    // Give up on this row if all the lifts are zero
-    if (row[assigned_weight_COL] === 0 && row[actual_weight_COL] === 0) return false;
-
-    // We are happy to approve this row as a valid squat lift
-    return true;
-}
-
-
 // Setup a charts.js chart.
-// Function assumes the global variable graphData already contains array of charts.js object entries such as:
-// {x:'2021-06-16', y:130, label:'3@119kg'}
 function getChartConfig () {
-    const data = {
-        datasets: [
-        {
-            label: 'Squat',
-            backgroundColor: 'rgb(255, 0, 0)',
-            borderColor: 'rgb(50, 50, 50)',
-            borderWidth: 3,
-            pointStyle: 'circle',
-            radius: 4,
-            hitRadius: 15,
-            hoverRadius: 10,
-            cubicInterpolationMode: 'monotone',
-            data: squatGraphData,
-        },
-        {
-            label: 'Deadlift',
-            backgroundColor: 'rgb(0, 255, 0)',
-            borderColor: 'rgb(50, 50, 50)',
-            borderWidth: 3,
-            pointStyle: 'circle',
-            radius: 4,
-            hitRadius: 15,
-            hoverRadius: 10,
-            cubicInterpolationMode: 'monotone',
-            data: deadliftGraphData,
-        },
-        {
-            label: 'Bench Press',
-            backgroundColor: 'rgb(0, 0, 255)',
-            borderColor: 'rgb(50, 50, 50)',
-            borderWidth: 3,
-            pointStyle: 'circle',
-            radius: 4,
-            hitRadius: 15,
-            hoverRadius: 10,
-            cubicInterpolationMode: 'monotone',
-            data: benchGraphData,
-        }
 
-    ]
+    colors = ['rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(100, 100, 0)', 'rgb(0, 100, 100)'];
+
+    // Make datasets of the first five lifts. FIXME: how would you do the 5 most popular lifts in the data? //
+    let dataSets = [];
+    for (let j = 0; j < 5; j++) {
+        dataSets.push({
+            label: processedData[j].name,
+            backgroundColor: colors[j],
+            borderColor: 'rgb(50, 50, 50)',
+            borderWidth: 3,
+            pointStyle: 'circle',
+            radius: 4,
+            hitRadius: 15,
+            hoverRadius: 10,
+            cubicInterpolationMode: 'monotone',
+            data: processedData[j].graphData,
+        });
+    }
+        
+    const data = {
+        datasets: dataSets, 
     };
 
     let delayed;
